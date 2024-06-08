419ef6f683d8c89619e83121502ae9a4a652f3a5
Your New Username <sahupravin960@gmail.com>
2024-06-08 21:50:45 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     // BASE_URL:"https://no-code-form-generator-backend.onrender.com",
-    BASE_URL: "http://localhost:3000",
+    BASE_URL: "https://snippetai-xi.vercel.app",
     // FRONTEND_BASE_URL:"https://formilo.vercel.app/"
   },
 };
