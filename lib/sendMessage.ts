50b8eaf310d1354db156fde315c046b61d64c370
Your New Username <sahupravin960@gmail.com>
2024-06-08 20:53:41 +0530

    api update

--- lib/sendMessage.ts
+++ lib/sendMessage.ts
@@ -1,9 +1,12 @@
 import axios, { Axios } from "axios";
 export const sendMessage = async (message: string) => {
   try {
-    const response = await axios.post("http://localhost:3000/api/gemini", {
-      messages: message,
-    });
+    const response = await axios.post(
+      "https://snippetai-xi.vercel.app/api/gemini",
+      {
+        messages: message,
+      }
+    );
     // console.log(response.data);
     return response.data;
   } catch (error) {
